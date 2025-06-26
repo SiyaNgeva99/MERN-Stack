a9482f8810ab2185ba608920dc50906979301117
SiyaNgeva99 <sbngeva@gmail.com>
2025-06-26 13:52:11 +0200

    new change

--- Chapter_06/movie-reviews/backend/server.js
+++ Chapter_06/movie-reviews/backend/server.js
@@ -1,15 +1,15 @@
-import express from 'express'
+import express from 'express' //import middleware
 import cors from 'cors'
-import movies from './api/movies.route.js'
+import movies from './api/movies.route.js' //import route
 
-const app = express()
+const app = express(); //We attach the cors and express.json middleware that express will use with:
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json()); //parsing middleware to enable the server to read and accept JSON in a request’s body
 
-app.use("/api/v1/movies", movies)
-app.use('*', (req, res) => {
-    res.status(404).json({ error: "not found" })
+app.use("/api/v1/movies", movies);
+app.use('*', (req,res)=>{
+res.status(404).json({error: "not found"})
 })
 
-export default app
+export default app;
